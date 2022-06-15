c7acfeb9f2dac64b596d79e3a7fd6e6c1f9a639f
vaspap1790 <vaspap1790@gmail.com>
2022-06-15 21:24:32 +0300

    fibonacci

---
+++ fibonacci.py
@@ -0,0 +1,29 @@
+def fib(n):
+    if n == 0: return 1
+    if n == 1: return 1
+    return fib(n - 1) + fib(n - 2)
+
+
+def fib_memoization(n, cache=None):
+    if n == 0: return 1
+    if n == 1: return 1
+
+    if cache is None: cache = {}
+    if n in cache: return cache[n]
+
+    result = fib_memoization(n - 1, cache) + fib_memoization(n - 2, cache)
+    cache[n] = result
+    return result
+
+
+def fib_bottom_up(n):
+    f1 = 1
+    f2 = 1
+    for i in range(2, n + 1):
+        f1, f2 = f2, f1 + f2
+    return f2
+
+
+print(fib(4))
+print(fib_memoization(4))
+print(fib_bottom_up(4))
